adacd309feb2eb39d7c8ee790c41206c570449ad
Risako <lilisako062397@gmail.com>
2023-11-26 13:26:59 +0000

    feat: add metrics to the logs page

---
+++ static/utils.js
@@ -0,0 +1,14 @@
+/*************************************************************************
+* Utils
+*************************************************************************/
+
+// Round a float to even with decimal places
+// Rounding logic: https://stackoverflow.com/a/49080858
+function round(n, places) {
+    if (typeof n !== 'number' || Number.isInteger(n)) { return n; }
+    var x = n * Math.pow(10, places);
+    var r = Math.round(x);
+    // Account for precision using Number.EPSILON
+    var br = (Math.abs(x) % 1 > 0.5 - Number.EPSILON && Math.abs(x) % 1 < 0.5 + Number.EPSILON) ? (r % 2 === 0 ? r : r - 1) : r;
+    return br / Math.pow(10, places);
+}
