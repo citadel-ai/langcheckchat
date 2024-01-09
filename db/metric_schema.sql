4946d8448ccc3680c01efa61fc387ff97e3f965b
Yosuke Higashi <yosuke.higashi1@gmail.com>
2024-01-09 06:25:47 +0000

    update db schema

---
+++ db/metric_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS metric (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    log_id INTEGER NOT NULL,
+    metric_name TEXT NOT NULL,
+    metric_value REAL,
+    FOREIGN KEY (log_id) REFERENCES chat(id)
+);
