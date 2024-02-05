0c3dafa7b65da34d61c21776d3f13475a092b0e0
risako <33516104+lilisako@users.noreply.github.com>
2024-02-06 00:24:47 +0900

    fix: direction for request_sentiment_openai
    Co-authored-by: Yosuke Higashi <107823399+yosukehigashi@users.noreply.github.com>

--- static/chat.js
+++ static/chat.js
@@ -154,7 +154,7 @@ const REFERENCE_FREE_METRICS = {
   'response_toxicity': { threshold: 0.5, direction: "low"},
   'response_toxicity_openai': { threshold: 0.5, direction: "low"},
   'request_sentiment': { threshold: 0.5, direction: "low"},
-  'request_sentiment_openai': { threshold: 0.5, direction: "low"},
+  'request_sentiment_openai': { threshold: 0.5, direction: "high"},
   'response_sentiment': { threshold: 0.5, direction: "high"},
   'response_sentiment_openai': { threshold: 0.5, direction: "high"},
   'request_fluency': { threshold: 0.5, direction: "high"},
